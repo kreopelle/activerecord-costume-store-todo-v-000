1448ac293fa06457f3e077933fe45dc76147a213
Kayla Reopelle <kreopelle@gmail.com>
2018-06-30 21:04:52 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,7 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
 
   def change
-    def create_table :haunted_houses do |t|
+    create_table :haunted_houses do |t|
       t.string :name
       t.string :location
       t.string :theme
